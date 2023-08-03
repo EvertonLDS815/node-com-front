44ceb5e79f13b151e3512e81bac1e1d218b17c4c
EvertonLDS815 <evertonlino1234@gmail.com>
2023-08-03 19:27:41 -0300

    add name and image on Schema

--- model/Product.js
+++ model/Product.js
@@ -1,28 +1,14 @@
-const {Schema, model} = require("mongoose");
-
-
+const { Schema, model } = require("mongoose");
 
 const loginSchema = new Schema({
-    name: {
-        type: String,
-        require: true
-    },
-    image: {
-        type: String,
-        require: true
-    },
-    description: {
-        type: String,
-        require: true
-    },
-    quantity: {
-        type: String,
-        require: true
-    },
-    code: {
-        type: String,
-        require: true
-    }
+  name: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
 });
 
 module.exports = model("Product", loginSchema);
